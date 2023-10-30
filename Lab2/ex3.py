ea0badc66eb3dbbd0530cd088c56c911495cd905
robert2407 <63233501+robert2407@users.noreply.github.com>
2023-10-30 12:22:07 +0200

    Update ex3.py

--- Lab2/ex3.py
+++ Lab2/ex3.py
@@ -2,10 +2,13 @@
 # a reunited with b, a - b, b - a)
 
 def list_operations(a, b):
-    intersection = list(set(a) & set(b))
-    union = list(set(a) | set(b))
-    a_minus_b = list(set(a) - set(b))
-    b_minus_a = list(set(b) - set(a))
+    intersection = [i for i in a if i in b] # Daca acea valoare i este si in a si in b o pune in intersection
+    union = list(a)
+    for x in b:
+        if x not in union:
+            union.append(x)
+    a_minus_b = [x for x in a if x not in b]
+    b_minus_a = [x for x in b if x not in a]
 
     return intersection, union, a_minus_b, b_minus_a
 
